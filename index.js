7a1a11a46f1322104494a2c2f2019072ff67e7f5
stasgga <stsggrn25@gmail.com>
2018-12-28 12:58:17 +0000

    initial commit, parses en-gb subscription confirmation email from itunes

---
+++ index.js
@@ -0,0 +1,28 @@
+const { readFile, writeFileSync } = require("fs");
+const { simpleParser } = require("mailparser");
+const { load } = require("cheerio");
+
+readFile("original_msg.txt", async (err, data) => {
+  // console.log(data.toString());
+  const parsed = await simpleParser(data);
+  // console.log(parsed.text);
+
+  const $ = load(parsed.html);
+
+  const [_, price, paymentMethod] = parsed.text.match(
+    /Subscription Price: (.+)Payment Method: (.+)/
+  );
+
+  const p = {
+    subscription: parsed.text.match(/Subscription: (.+)/)[1],
+    app: parsed.text.match(/App: (.+)/)[1],
+    img: $("img.large-artwork").attr("src"),
+    dop: new Date(parsed.text.match(/Date of Purchase: (.+)/)[1]),
+    price,
+    paymentMethod,
+    cancelBy: new Date(parsed.text.match(/unless cancelled by (.+)./)[1])
+  };
+  console.log(p);
+
+  // writeFileSync("parsed.html", parsed.html);
+});
